6231f61f43fda618ff9cbfacc68e587dae4081ac
Edwin Rivera <waehake@gmail.com>
2018-03-23 23:50:07 -0400

    created the filterTask, clearTask implementations

--- task_site/app.js
+++ task_site/app.js
@@ -15,6 +15,10 @@ function loadEventListeners() {
     form.addEventListener('submit', addTask);
     //add event listener to taskList
     taskList.addEventListener('click', removeTask);
+    //clear task event
+    clearBtn.addEventListener('click', clearTasks);
+    //filter task events
+    filter.addEventListener('keyup', filterTasks);
 }
 
 //to add a task
@@ -52,6 +56,39 @@ function removeTask(e) {
     //use e.target to get a reference to what was clicked
     console.log(e.target.parentElement.parentElement);
     //remove the element by targeting the parent elements parent element
-    e.target.parentElement.parentElement.remove();
+    if (e.target.parentElement.classList.contains('delete-item')) {
+        //add a confirmation
+        if(confirm('Are you sure?')) {
+            //remove the element by targeting the parent elements parent element
+            e.target.parentElement.parentElement.remove();
+        }
+    }
     e.preventDefault();
+}
+
+//Clear task implementation
+function clearTasks() {
+    // not preferred method
+    // taskList.innerHTML = '';
+
+    //preferred method for removing elements
+    while (taskList.firstChild) {
+            taskList.removeChild(taskList.firstChild);
+    }
+}
+
+//filtering task implementation
+function filterTasks(e) {
+    //reference to the value that is being typed
+    const text = e.target.value.toLowerCase();
+    //target all of the items with collection and iterate through each item
+    document.querySelectorAll('.collection-item').forEach(function(task) {
+        const item = task.firstChild.textContent;
+        if (item.toLowerCase().indexOf(text) != -1) {
+            //display the item because there is a match
+            task.style.display = 'block';
+        } else {
+            task.style.display = 'none';
+        }
+    });
 }
