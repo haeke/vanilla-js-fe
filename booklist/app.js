487e70ab08ad19d53ae4a657d59b8f162c28452d
Edwin Rivera <waehake@gmail.com>
2018-03-27 09:57:29 -0400

    added a method to clear the three inputs after submitting the form

--- booklist/app.js
+++ booklist/app.js
@@ -8,14 +8,35 @@ function Book(title, author, isbn) {
 
 
 //implementation
-function interface() {
-    return;
+function UI() {
 };
 
+//clear the booklist fields
+UI.prototype.clearFields = function() {
+    //set the values of the three fields to nothing
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
+    document.querySelector('#isbn').value = '';
+}
+
+//add book to the list
+UI.prototype.addBookToList = function(book) {
+    console.log(book);
+    //target the book element
+    const list = document.querySelector('#book-list');
+    //create a table row element
+    const row = document.createElement('tr');
+    //insert the columns
+    row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="delete">x</a></td>`;
+    //append to the row
+    list.appendChild(row);
+}
+
 //event listeners
-
-
-
 let bookForm = document.querySelector('#book-form');
 
 bookForm.addEventListener('submit', function(e) {
@@ -27,7 +48,13 @@ bookForm.addEventListener('submit', function(e) {
     const isbn = document.querySelector('#isbn').value;
     //create a book object from the form values
     const book = new Book(title, author, isbn);
-    console.log(`${book.title} - ${book.author} - ${book.isbn}`);
+
+    //create a UI object
+    const ui = new UI();
+    //add book to the UI list
+    ui.addBookToList(book);
+    //clear the UIfields
+    ui.clearFields(title, author, isbn);
     //don't refresh on submit
     e.preventDefault();
 });
