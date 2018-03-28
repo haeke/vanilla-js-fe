b754cc9c9ab74d0ba00609ea51b17402e692f311
Edwin Rivera <waehake@gmail.com>
2018-03-28 10:08:36 -0400

    added a function to display the jokes that are generated, added errorhandling if the form is empty or if there is a reponse error

--- quote_site/app.js
+++ quote_site/app.js
@@ -9,19 +9,43 @@ function getJokes(e) {
     //target the number input type
     const number = document.querySelector('input[type="number"]').value;
     console.log(number);
+    //use output to store what we want to place into the DOM
+    let output = '';
     //use xmlhttprequest
     const xhr = new XMLHttpRequest();
-    //make a get request to the api
-    xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
-    //if the request was successful - log it to the console for now
-    xhr.onload = function() {
-        if (this.status === 200) {
-            //parse the JSON to create an object
-            const response = JSON.parse(this.responseText);
-            console.log(response.value);
+    if (number > 0) {
+        console.log(number);
+        //make a get request to the api
+        xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
+        //if the request was successful - log it to the console for now
+        xhr.onload = function() {
+            if (this.status === 200) {
+                //parse the JSON to create an object
+                const response = JSON.parse(this.responseText);
+                console.log(response.value);
+                //determine what to display based on
+                //whether the request was successful or not
+                if (response.type === 'success') {
+                    //append the jokes to the output variable
+                    response.value.forEach(joke => {
+                        //create the list of jokes
+                        output += `<li>${joke.joke}</li>`;
+                    });
+                } else {
+                    //the reponse was not successful
+                    output += '<li>Something went wrong</li>';
+                }
+                document.querySelector('.jokes').innerHTML = output;
+            }
         }
+    } else {
+        //handle the case where someone tries to enter an empty form
+        output += '<li>You need to enter a number</li>';
+        document.querySelector('.jokes').innerHTML = output;
     }
-    xhr.send();
-
+    //only make a send request of the readyStatus is 1
+    if (xhr.readyState === 1) {
+        xhr.send();
+    }
     e.preventDefault();
 }
